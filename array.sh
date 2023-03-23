1471d60b5cba6810e65a2f32fdf8117acad60a1c
AbDevillier <abhaykantekar@gmail.com>
2023-03-23 13:26:33 +0530

    2nd commit from ubu tu

---
+++ array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+namelist=(ab az ef gh);
+          
+echo "first index: " ${namelist[0]}
+echo "second index: " ${namelist[1]}
+echo "third index: " ${namelist[2]}
+echo "fourth index: " ${namelist[3]}
+
